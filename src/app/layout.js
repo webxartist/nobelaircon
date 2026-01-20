967d8956309cc455b0bffb4832c81bf181831a83
webxartist <webxartist@gmail.com>
2026-01-21 00:53:23 +0530

    update4

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="w1i12-RFUp7G8dorx01XbQO24cw_QfBeA7ElcFylFyk"
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
